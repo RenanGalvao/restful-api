f0bc9a3e1c40d66c888eb5fc800d3184e5e4c2e8
Renan M Galvão <renan.m.galvao@gmail.com>
2021-02-09 08:08:26 -0300

    Sensitive data out of config

---
+++ config _sample.js
@@ -0,0 +1,39 @@
+// Container for all the environments
+let environments = {};
+
+// Development (default)
+environments.development = {
+  httpPort: 3000,
+  httpsPort: 3001,
+  envName: 'development',
+  hashingSecret: 'my little secret',
+  maxChecks: 5,
+  twilio: {
+    accountSid: 'your_account_sid',
+    authToken: 'your_auth_token',
+    fromPhone: 'your_phone'
+  },
+};
+
+// Production
+environments.production = {
+  httpPort: 5000,
+  httpsPort: 5001,
+  envName: 'production',
+  hashingSecret: 'my little secret',
+  maxChecks: 5,
+  twilio: {
+    accountSid: 'your_account_sid',
+    authToken: 'your_auth_token',
+    fromPhone: '+your_phone'
+  },
+};
+
+
+// Determine wich environment was passed as a command-line argument if any
+const currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// Check that the current environment is one of the environments above, if not, default to development
+const environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment]: environments.development;
+
+module.exports = environmentToExport;
