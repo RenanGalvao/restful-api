73a71ffe4c7c0be42d53e58b7dcb32e5c89eb535
Renan M Galvão <renan.m.galvao@gmail.com>
2021-02-17 22:51:20 -0300

    Added session create via GUI

--- public/js/app.js
+++ public/js/app.js
@@ -50,7 +50,11 @@ app.client.request = (options = app.client.request._optionsDefault, payload = {}
 
 // Bind the forms
 app.bindForms = function(){
-  document.getElementById('accountCreate').addEventListener('submit', async function(e){
+  if(!document.querySelector('form')){
+    return;
+  }
+
+  document.querySelector('form').addEventListener('submit', async function(e){
 
     // Stop it from submitting
     e.preventDefault();
@@ -82,7 +86,7 @@ app.bindForms = function(){
     const [statusCode, response] = await app.client.request(options, payload); 
     
     // Display an error on the form if needed
-    if(statusCode !== 201){
+    if(statusCode !== 201 && statusCode !== 200){
 
       // Try to get the error from the api, or set a default error message
       const error = response.message || 'An error has occured, please try again';
@@ -101,18 +105,110 @@ app.bindForms = function(){
 };
 
 // Form response processor
-app.formResponseProcessor = function(formId, requestPayload, responsePayload){
-  const functionToCall = false;
+app.formResponseProcessor = async function(formId, requestPayload, responsePayload){
+  let functionToCall = false;
+
+  /*
+  * Account
+  */
+
+  // If account creation was successful, try to immediately log the user in
   if(formId == 'accountCreate'){
-    // @TODO Do something here now that the account has been created successfully
-    alert(responsePayload);
+    // Take the phone and password, and use it to log the user in
+    const newPayload = {
+      'phone' : requestPayload.phone,
+      'password' : requestPayload.password
+    };
+
+    const options = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      path: '/api/login',
+      method: 'POST',
+    };
+    const [statusCode, response] = await app.client.request(options, newPayload);
+    
+    // Display an error on the form if needed
+    if(statusCode !== 200){
+
+      // Try to get the error from the api, or set a default error message
+      const error = response.message || 'An error has occured, please try again';
+
+      // Set the formError field with the error text
+      document.querySelector(`#${formId} .formError`).innerHTML = error;
+
+      // Show (unhide) the form error field on the form
+      document.querySelector(`#${formId} .formError`).style.display = 'block';
+
+    } else {
+      // If successful, set the token and redirect the user
+      app.setSessionToken(response);
+      window.location = '/checks/all';
+    }
+  }
+
+  /*
+  * Session
+  */
+
+  // If login was successful, set the token in localstorage and redirect the user
+  if(formId == 'sessionCreate'){
+    app.setSessionToken(responsePayload);
+    window.location = '/checks/all';
+  }
+};
+
+// Get the session token from localstorage and set it in the app.config object
+app.getSessionToken = function(){
+  const tokenString = localStorage.getItem('token');
+  if(typeof tokenString  == 'string'){
+    try{
+      const token = JSON.parse(tokenString);
+      app.config.sessionToken = token;
+      if(typeof token  == 'object'){
+        app.setLoggedInClass(true);
+      } else {
+        app.setLoggedInClass(false);
+      }
+    }catch(e){
+      app.config.sessionToken = false;
+      app.setLoggedInClass(false);
+    }
+  }
+};
+
+// Set the session token in the app.config object as well as localstorage
+app.setSessionToken = function(token){
+  app.config.sessionToken = token;
+  const tokenString = JSON.stringify(token);
+  localStorage.setItem('token',tokenString);
+  if(typeof token == 'object'){
+    app.setLoggedInClass(true);
+  } else {
+    app.setLoggedInClass(false);
+  }
+};
+
+// Set (or remove) the loggedIn class from the body
+app.setLoggedInClass = function(add){
+  var target = document.querySelector("body");
+  if(add){
+    target.classList.add('loggedIn');
+  } else {
+    target.classList.remove('loggedIn');
   }
 };
 
 // Init (bootstrapping)
 app.init = function(){
+
   // Bind all form submissions
   app.bindForms();
+
+  // Get the token from localstorage
+  app.getSessionToken();
+
 };
 
 // Call the init processes after the window loads
